3216419ddeb1d26e69926d9bab5769c6d5e86c6d
JustineAubert <justine.aubert@u-psud.fr>
2020-06-22 15:30:39 +0200

    commit

--- src/application/Earth.java
+++ src/application/Earth.java
@@ -1,16 +1,16 @@
 package application;
 
 public class Earth {
-//
-//	public static void print() {
-//		
-//		
-//			System.out.println(Search.getInstance().findByYear(1952));
-//			System.out.println(Search.getInstance().findByYearAndByLongAndLat(1881, "12", "154"));
-//			System.out.println(Search.getInstance().getTempMax());
-//			System.out.println(Search.getInstance().getTempMin());
-//			System.out.println(Search.getInstance().findByLongAndLat("12","154"));
-//		
-//
-//}
+
+	public static void print() {
+		
+		
+			System.out.println(Search.getInstance().findByYear(1952));
+			System.out.println(Search.getInstance().findByYearAndByLongAndLat(1881, "12", "154"));
+			System.out.println(Search.getInstance().getTempMax());
+			System.out.println(Search.getInstance().getTempMin());
+			System.out.println(Search.getInstance().findByLongAndLat("12","154"));
+		
+
+}
 	}
